773970fa01066dd9a3bc2dd818958e430838ea6c
Aleksei Berezkin <16083785+aleksei-berezkin@users.noreply.github.com>
2020-08-06 22:16:03 +0300

    Base: different input combinations

--- src/impl/base.test.ts
+++ src/impl/base.test.ts
@@ -1,40 +1,64 @@
 import { twice } from './testUtil';
 import { BaseImpl } from './base';
+import { appendReturned } from './util';
 
-function of<T>(...items: T[]) {
+function of<T>(head: T[], tail?: T[]) {
     return new BaseImpl(undefined, function* () {
-        yield *items;
+        yield* head;
+        if (tail) {
+            return {
+                array: tail,
+                canModify: false,
+            }
+        }
     });
 }
 
+function forInput<T>(input: T[], run: (base: BaseImpl<unknown, T>) => void) {
+    for (let headSize = 0; headSize <= input.length; headSize++) {
+        run(of(input.slice(0, headSize), input.slice(headSize)));
+    }
+    twice(() => run(of(input)));
+}
+
 test('BaseImpl size', () => {
-    const b = of('a', 'a', 'a');
-    twice(() => expect(b.size()).toEqual(3));
+     forInput(
+         ['a', 'a', 'a'],
+         b => twice(() => expect(b.size()).toEqual(3))
+     );
 });
 
 test('BaseImpl composition', () => {
-    const b1 = of('a', 'b', 'c');
-    const b2 = new BaseImpl(b1, function* (items) {
-        yield *items;
-        yield 'd';
-    });
-    twice(() => expect([...b2]).toEqual(['a', 'b', 'c', 'd']));
+    forInput(
+        ['a', 'b', 'c'],
+        b => {
+            const bb = new BaseImpl(b, function* (gen) {
+                yield* appendReturned(gen);
+                yield 'd';
+            });
+            twice(() => expect([...bb]).toEqual(['a', 'b', 'c', 'd']));
+        }
+    );
 });
 
 test('BaseImpl composition to other', () => {
-    const b = of('a', 'b', 'c');
-    const bb1 = new BaseImpl(b, function* (items) {
-        yield *items;
-        yield 'd';
-    });
-    const bb2 = new BaseImpl(b, function* (items) {
-        for (const i of items) {
-            yield i.toUpperCase();
+    forInput(
+        ['a', 'b', 'c'],
+        b => {
+            const bb1 = new BaseImpl(b, function* (gen) {
+                yield* appendReturned(gen);
+                yield 'd';
+            });
+            const bb2 = new BaseImpl(b, function* (gen) {
+                for (const i of appendReturned(gen)) {
+                    yield i.toUpperCase();
+                }
+            });
+            
+            twice(() => {
+                expect([...bb1]).toEqual(['a', 'b', 'c', 'd']);
+                expect([...bb2]).toEqual(['A', 'B', 'C']);
+            });
         }
-    });
-
-    twice(() => {
-        expect([...bb1]).toEqual(['a', 'b', 'c', 'd']);
-        expect([...bb2]).toEqual(['A', 'B', 'C']);
-    });
+    );
 });
